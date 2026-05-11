ba5f98b8e6b71e94506f0b200dcba3f06c170da1
NickS <155711138+JollyRodgerNAS@users.noreply.github.com>
2026-05-10 19:27:14 -0500

    Added voltage calculation

--- ElectricianHelper.c
+++ ElectricianHelper.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int ohmsLawCalculator(void);
+
 int main(void) 
 {
 	printf("Welcome to Electrician Helper!\n");
@@ -17,18 +19,24 @@ int main(void)
 
 		if (result != 1) 
 		{
-			printf("Invalid input\n");
-			return 0;
+			printf("Invalid input! Try again...\n");
+		        int c;
+			while ((c = getchar()) != '\n' && c != EOF);
+			continue;
+	
 		}
 
 		switch(userInput) 
 		{
 			case 1: 
 			{
-				printf("1. Solve for Voltage\n");
-				printf("2. Solve for Current\n");
-				printf("3. Solve for Resistance\n");
-				printf("4. Back\n");
+				int result = ohmsLawCalculator();
+				if (result == -1) 
+				{
+					printf("Error!\n");
+					return 0;
+				}
+				printf("Your voltage is: %d\n", result);
 				return 0;
 			}
 			
@@ -46,5 +54,61 @@ int main(void)
 			return 0;
 		}
 	}
+	return 0;
+}
+
+int ohmsLawCalculator(void) 
+{
+	printf("1. Solve for Voltage\n");
+	printf("2. Solve for Current\n");
+	printf("3. Solve for Resistance\n");
+
+	int isActive = 1;
+	int userInput = 0;
+
+	while (isActive) 
+	{
+		int result = scanf("%d", &userInput);
+		
+		if (result != 1) 
+		{
+			printf("Invalid input! Try again...\n");
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF);
+			continue;
+		}
+		else 
+		{
+			switch(userInput) 
+			{
+				case 1:
+				{
+					int voltage = 0;
+					int current = 0;
+					int resistance = 0;
+					printf("Enter your Current(Amperes): ");
+					scanf("%d", &current);
+					printf("\n");
+					printf("Enter your Resistance(Ohms): ");
+					scanf("%d", &resistance);
+					printf("\n");
+
+					voltage = current * resistance;
+
+					return voltage;
+				}
+			
+				default: 
+				{
+					printf("Please select a valid option!\n");	
+					continue;
+				}
+			}
+
+		}	
+
+		return -1;
+	}
+	
 	return 0;
 }
